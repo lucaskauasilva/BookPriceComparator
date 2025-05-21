dd6ab300f1e6b43990ed47f32a51ae5d2bf0e80a
Lucas <lukaskaua172003@gmail.com>
2025-05-21 11:07:11 -0300

    Atualização do Código:
    - Documentação com javadoc;
    
    - Melhoria na exibição da comparação dos livros;
    
    - Adição de variáveis que comparam os livros.

--- src/core/Livro.java
+++ src/core/Livro.java
@@ -10,6 +10,16 @@ public class Livro implements Comparable<Livro>{
 		this.custo = custo;
 	}
 
+	/**
+	 *Esse método compara o preço de dois livros, e se a diferença de preço for menor que um determinado valor limite (0.001), 
+	 *então, o resultado 0 será retornado, indicando que os dois livros têm o mesmo preço. 
+	 *Essa comparação foi feita dessa forma pois a comparação de valores de ponto flutuante tem algumas limitações no computador. 
+	 *Se o preço do livro corrente (atual) for maior do que o preço do livro passado como argumento, então, o resultado 1 será retornado, 
+	 *caso contrário, se o preço do livro corrente for menor do que o preço do livro passado como argumento, o resultado -1 será retornado.  
+	 *@return 0 se ambos os livros possuirem o mesmo preço.
+	 *@return 1 se o livro passado como argumento for mais barato do que o livro corrente.
+	 *@return -1 se o livro corrente for mais barato do que o livro passado como argumento
+	 */
 	@Override
 	public int compareTo(Livro livro) {
 		
@@ -20,6 +30,5 @@ public class Livro implements Comparable<Livro>{
 		} else {
 			return -1;
 		}
-		
 	}
 }
